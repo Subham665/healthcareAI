01426b6e967c00dfe13516d5a18f4869a1dbd4e5
Soumen Singh <Soumensingh951@gmail.com>
2023-09-15 10:37:21 +0530

    change something

--- src/context.jsx
+++ src/context.jsx
@@ -3,7 +3,7 @@ import reducer from "./reducer";
 
 const AppContext = React.createContext();
 
-const API = "https://thapareactapi.up.railway.app";
+const API = "https://up.railway.app";
 
 const intialState = {
   name: "",
